198b0088d4ace84e39adca4ed1edf000c5b26955
kamaro3 <38434334+kamaro3@users.noreply.github.com>
2024-10-21 23:01:46 -0300

    Update Board.java- solucion de pregunta 3

--- src/main/java/com/c0nrad/darkchess/models/Board.java
+++ src/main/java/com/c0nrad/darkchess/models/Board.java
@@ -63,7 +63,7 @@ public class Board {
 
             for (int x = 0; x < Board.BOARD_SIZE; x++) {
                 String pieceStr = line.substring(x, x+1);
-                if (pieceStr.equals(" ")) {
+                if ("".equals(variable)) {
                     continue;
                 } else {
                     try {
